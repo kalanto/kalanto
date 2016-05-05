5e4b03f874d371a9484e663eef7e5d59960b370d
kalanto <sandrewbooth@gmail.com>
2016-05-04 20:30:32 -0600

    clean up server files

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ var mongoose = require('mongoose');
 var cors = require ('cors');
 var bodyParser=require('body-parser');
 // var FacebookStrategy = require('passport-facebook').Strategy;
-var keys = require('./kalanto/keys.js');
-var Customer = require('./public/js/customer/customer.js');
+var keys = require('../kalanto/keys.js');
+var Customer = require('../public/js/customer/customer.js');
+var endPointControl = require ('./endPointControl.js');
 //application and database setup
 var app = express();
 console.log(__dirname);
@@ -20,56 +21,13 @@ app.use(bodyParser.json());
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/database');
 
-//end points
-app.post('/customers/add', function(req, res){
-   console.log(req.body);
-   var customer = new Customer(req.body);
-   customer.save(function(err, status){
-      console.log(err);
-      return err ? res.status(500).send(err) : res.send(status);
-   });
-});
-app.get('/customers/get', function(req, res, next){
-   var query = req.query;
-   Customer.find(query, function(err, response){
-      if(err){
-         res.status(500).json(err);
-      }
-      else{
-         res.json(response);
-      }
-   });
-});
-app.get('/customers/get/:id', function(req,res,next){
-   Customer.findOne(req.params.id, function(err, response){
-      if(err){
-         res.status(500).send(err);
-      }
-      else{
-         res.send(response);
-      }
-   });
-});
-app.put('/customers/put/:id', function(req, res, next){
-   Customer.findOneAndUpdate(req.params.id, function(err, response){
-      if(err){
-         res.status(500).send(err);
-      }
-      else{
-         res.send(response.data);
-      }
-   });
-});
-app.delete('/customers/delete/:id', function(req,res,next){
-   Customer.findOneAndRemove(req.query, function(err,response){
-      if(err){
-         return res.status(500).send(err);
-      }
-      else{
-         return res.send(response);
-      }
-   });
-});
+//end points in controller
+app.post('/customers/add', endPointControl.postCustomer);
+app.get('/customers/get', endPointControl.getCustomer);
+app.get('/customers/get/:id',endPointControl.getOneCustomer);
+app.put('/customers/put/:id', endPointControl.updateCustomer);
+app.delete('/customers/delete/:id', endPointControl.deleteCustomer);
+
 
 //facebook Login code
 // passport.use(new FacebookStrategy({
