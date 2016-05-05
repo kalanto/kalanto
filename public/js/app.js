ed10c60989d2c4b3bc5cc756739e6694243088d0
kalanto <sandrewbooth@gmail.com>
2016-05-05 11:07:49 -0600

    add ui.router

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,39 @@
-angular.module('kalanto', []);
+angular.module('kalanto', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider){
+   $httpProvider.interceptors.push('httpRequestInterceptor');
+
+   //routing
+   $stateProvider
+      .state('home', {
+         url: '/',
+         templateUrl: '../views/home.html',
+         controller: 'insert controller here'
+      })
+      .state('add', {
+         url: '/customers/add',
+         templateUrl: '../views/add.html',
+         controller: 'insert file here'
+      })
+      .state('edit',{
+         url: '/customers/put/:id',
+         templateUrl: '../views/edit.html',
+         controller: 'insert file here'
+      })
+      .state('delete',{
+            url: '/customers/delete/:id',
+            templateUrl: '../views/delete.html',
+            controller: 'insert file here'
+      })
+      .state('getCustomers',{
+         url: '/customers/get/',
+         templateUrl: '../views/get.html',
+         controller: 'insert file here'
+
+      })
+      .state('getOneCustomer', {
+         url: '/customers/get/:id',
+         templateUrl: '../views/getOne.html',
+         controller: 'insert file here'
+      });
+});
