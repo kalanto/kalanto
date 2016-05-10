9e02e1417bc45a42448a47655eb608b827205298
kalanto <sandrewbooth@gmail.com>
2016-05-10 15:28:01 -0600

    add get route for clients who owe

--- public/js/app.js
+++ public/js/app.js
@@ -5,33 +5,35 @@ angular.module('kalanto', ['ui.router'])
 //routing
    $stateProvider
       .state('home', {
-         url: '/',
-         templateUrl: 'routes/home.html',
+         url: '/'
+      ,  templateUrl: 'routes/home.html'
       })
       .state('add', {
-         url: '/customers/add',
-         templateUrl: 'routes/add.html',
-         // controller: 'insert file here'
+         url: '/customers/add'
+      ,  templateUrl: 'routes/add.html'
       })
       .state('edit',{
-         url: '/customers/put/:id',
-         templateUrl: 'routes/edit.html',
-         // controller: 'insert file here'
+         url: '/customers/put/:id'
+      ,  templateUrl: 'routes/edit.html'
       })
+      .state('get',{
+         url: '/customers/get'
+      ,  templateUrl: 'routes/get.html'
+      })
+      .state('owe',{
+         url: '/customers/owe'
+      ,  templateUrl: 'routes/owe.html'
+      })
+      // .state('getOneCustomer', {
+      //    url: '/customers/get/:id',
+      //    templateUrl: '../routes/getOne.html',
+      //    // controller: 'insert file here'
+      // })
       // .state('delete',{
       //       url: '/customers/delete/:id',
       //       templateUrl: '../routes/delete.html',
       //       // controller: 'insert file here'
       // })
-      .state('get',{
-         url: '/customers/get',
-         templateUrl: 'routes/get.html',
-         // controller: 'insert file here'
-      });
-      // .state('getOneCustomer', {
-      //    url: '/customers/get/:id',
-      //    templateUrl: '../routes/getOne.html',
-      //    // controller: 'insert file here'
-      // });
+
       $urlRouterProvider.otherwise('/')
 });
