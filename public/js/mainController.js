60493a8560c396dc3edb4f2751caee9ef3da5c52
kalanto <sandrewbooth@gmail.com>
2016-05-09 19:16:48 -0600

    added email, debt, paydate to customer data object

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -1,19 +1,13 @@
 angular.module('kalanto').controller('kalantoController', function($scope, kalantoService){
-   $scope.customer={
-      firstname: "",
-      lastname: "",
-      address:"",
-      city:"",
-      state:"",
-      zipCode:"",
-      phone:{areaCode:"",
-      ANI:""}
-   };
-   $scope.getCustomers=function(){
-      kalantoService.getCustomers().then(function(response){
-         $scope.clients = response;
-      });
-   };
+
+$scope.getCustomers=function(){
+   kalantoService.getCustomers().then(function(response){
+      $scope.clients = response;
+         console.log(response);
+   });
+
+};
+   $scope.getCustomers();
 // gets customer by ID
 $scope.getOneCustomer = function(customer){
    kalantoService.getOneCustomer(customer);
@@ -21,6 +15,21 @@ $scope.getOneCustomer = function(customer){
 //adds a customer to the database
    $scope.addCustomer = function(customer){
       kalantoService.addCustomer(customer);
+      $scope.customer = {
+         firstname: "",
+         lastname: "",
+         address:"",
+         city:"",
+         state:"",
+         zipCode:"",
+         phone: {
+            areaCode:"",
+            ANI:""
+         },
+         email:"",
+         debt:"",
+         payDate:""
+      };
    };
 //updates a customer in the database by ID/selection click event
 $scope.updateCustomer = function(selected){
